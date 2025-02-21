3426b7df9edaddeff91f4b355252c27ce71d2197
cottozen <christian.ottosen02@gmail.com>
2025-02-21 15:40:06 +0100

    optimize

--- 2024/day20/main.py
+++ 2024/day20/main.py
@@ -1,7 +1,5 @@
-from collections import deque
-from queue import PriorityQueue
-from typing import DefaultDict, NamedTuple
-import copy
+import numpy as np
+from typing import NamedTuple
 
 
 type Grid = list[list[str]]
@@ -18,23 +16,12 @@ def parse_input(file_path: str) -> Grid:
         rows = file.readlines()
         for row in rows:
             grid.append([c for c in row.strip()])
-    # remove out layer of walls
     return grid
 
 
 def display_grid(grid: Grid):
     for row in grid:
         print("".join(row))
-
-
-def display_cheat_path(cheat_path: list[GridPos], grid: Grid):
-    start, end = find_start_and_end(grid)
-    grid_copy = copy.deepcopy(grid)
-    for i, p in enumerate(cheat_path):
-        grid_copy[p.row][p.col] = str(i)
-    grid_copy[start.row][start.col] = "S"
-    grid_copy[end.row][end.col] = "E"
-    display_grid(grid_copy)
 
 
 def find_start_and_end(grid: Grid) -> tuple[GridPos, GridPos]:
@@ -63,163 +50,72 @@ def get_neighbours(p: GridPos, grid: Grid) -> list[GridPos]:
     return neighbours
 
 
-def dijkstra(start: GridPos, grid: Grid):
-    q = PriorityQueue()
-    q.put((0, start))
-    prev: dict[GridPos, GridPos] = dict()
-    visited: set[GridPos] = set()
-    distances = DefaultDict(lambda: float("inf"))
-    distances[start] = 0
-
-    while not q.empty():
-        dist, curr = q.get()
-        if curr in visited:
-            continue
-        visited.add(curr)
-
-        neighbours = get_neighbours(curr, grid)
-        for n in neighbours:
-            if grid[n.row][n.col] == "#":
-                continue
-            new_dist = dist + 1
-            q.put((new_dist, n))
-
-            if distances[n] > new_dist:
-                distances[n] = new_dist
-                prev[n] = curr
-    return prev, distances
-
-
-def construct_path(end: GridPos, prev: dict[GridPos, GridPos]) -> list[GridPos]:
-    curr = end
-    path = []
-    while curr:
-        path.append(curr)
-        curr = prev.get(curr)
-    return list(reversed(path))
-
-
-def find_cheat_end_positions(
+def find_cheats(
     cheat_start: GridPos,
+    path: dict[GridPos, int],
     grid: Grid,
     max_distance: int,
+    target: int,
 ):
-    q = deque([(cheat_start, 0)])
-    visited: set = {cheat_start}
-    cheat_end_positions: list[tuple[GridPos, int]] = []
-    prev: dict[GridPos, GridPos] = {}
-    while q:
-        curr, dist = q.popleft()
-        if dist > max_distance:
-            continue
+    num_cheats = 0
+
+    pre_walked_steps = path[cheat_start]
+
+    max_row = min(cheat_start.row + max_distance, len(grid) + 1)
+    max_col = min(cheat_start.col + max_distance, len(grid[0]) + 1)
+    min_row = max(cheat_start.row - max_distance, 0)
+    min_col = max(cheat_start.col - max_distance, 0)
+    for i in range(min_row, max_row + 1):
+        row_offset = min(abs(cheat_start.row - i), 0)
+        for j in range(min_col + row_offset, max_col + 1 - row_offset):
+            cheat_end = GridPos(i, j)
+            if cheat_end not in path:
+                continue
+            dy, dx = abs(cheat_end.row - cheat_start.row), abs(
+                cheat_end.col - cheat_start.col
+            )
+            dist = dy + dx
+            cheat_value = path[cheat_end] - dist - pre_walked_steps
+            if dist <= max_distance and cheat_value >= target:
+                num_cheats += 1
+    return num_cheats
+
+
+def dfs(grid: Grid):
+    start, _ = find_start_and_end(grid)
+    path = []
+    stack = [start]
+    visited = set()
+    while len(stack):
+        curr = stack.pop()
+        path.append(curr)
+        visited.add(curr)
         neighbours = get_neighbours(curr, grid)
         for n in neighbours:
-            entry = (n, dist + 1)
-            if n == cheat_start:
+            if n in visited or grid[n.row][n.col] != ".":
                 continue
-            if n in visited:
-                continue
-            prev[n] = curr
-            visited.add(n)
-            q.append(entry)
-        if curr != cheat_start and grid[curr.row][curr.col] != "#":
-            cheat_end_positions.append((curr, dist))
-
-    return cheat_end_positions
+            stack.append(n)
+    return path
 
 
-def find_cheat_paths(
-    grid: Grid, cheat_dist: int, min_benefit: int, use_exact_match: bool = False
-):
-
-    start, end = find_start_and_end(grid)
-    prev, travel_distances = dijkstra(start, grid)
-    path = construct_path(end, prev)
-    worse_path_len = travel_distances[end]
-
-    cheat_paths = set()
-    for cheat_start in path:
-        cheat_end_positions = find_cheat_end_positions(cheat_start, grid, cheat_dist)
-        for cheat_end, cheat_dist in cheat_end_positions:
-            left = worse_path_len - travel_distances[cheat_end]
-            traveled = travel_distances[cheat_start]
-            new_dist = traveled + left + cheat_dist
-            is_valid = (
-                (worse_path_len - new_dist) == min_benefit
-                if use_exact_match
-                else (worse_path_len - new_dist) >= min_benefit
-            )
-            if is_valid:
-                cheat_paths.add((cheat_start, cheat_end))
-    return cheat_paths
-
-
-TESTS_PART_ONE = [
-    (14, 2, 2),
-    (14, 4, 2),
-    (2, 6, 2),
-    (4, 8, 2),
-    (2, 10, 2),
-    (3, 12, 2),
-    (1, 20, 2),
-    (1, 36, 2),
-    (1, 38, 2),
-    (1, 40, 2),
-    (1, 64, 2),
-]
-
-TESTS_PART_TWO = [
-    (32, 50, 20),
-    (31, 52, 20),
-    (29, 54, 20),
-    (39, 56, 20),
-    (25, 58, 20),
-    (23, 60, 20),
-    (20, 62, 20),
-    (19, 64, 20),
-    (12, 66, 20),
-    (14, 68, 20),
-    (12, 70, 20),
-    (22, 72, 20),
-    (4, 74, 20),
-    (3, 76, 20),
-]
-
-
-def test():
-    grid = parse_input("example.txt")
-    print("TEST PART ONE")
-    for expected, picoseconds, num_cheat_steps in TESTS_PART_ONE:
-        cheat_paths = find_cheat_paths(
-            grid, num_cheat_steps, picoseconds, use_exact_match=True
-        )
-        try:
-            assert (
-                len(cheat_paths) == expected
-            ), f"failed on {expected} != {len(cheat_paths)}, {expected, picoseconds, num_cheat_steps}"
-        except Exception as ex:
-            print(ex)
-    print("TEST PART TWO")
-    for expected, picoseconds, num_cheat_steps in TESTS_PART_TWO:
-        cheat_paths = find_cheat_paths(
-            grid, num_cheat_steps, picoseconds, use_exact_match=True
-        )
-        try:
-            assert (
-                len(cheat_paths) == expected
-            ), f"failed on {expected} != {len(cheat_paths)}, {expected, picoseconds, num_cheat_steps}"
-        except Exception as ex:
-            print(ex)
+def solve(grid: Grid, cheat_dist: int, min_benefit: int):
+    path = dfs(grid)
+    path_dist = {p: i for i, p in enumerate(path)}
+    return np.sum(
+        [
+            find_cheats(cheat_start, path_dist, grid, cheat_dist, min_benefit)
+            for cheat_start in path
+        ]
+    )
 
 
 def main():
     grid = parse_input("input.txt")
-    cheat_paths = find_cheat_paths(grid, 2, 100)
-    print("RESULT: ", len(cheat_paths))
-    cheat_paths = find_cheat_paths(grid, 20, 100)
-    print("RESULT PART TWO: ", len(cheat_paths))
+    num_cheats = solve(grid, 2, 100)
+    print("RESULT: ", num_cheats)
+    num_cheats = solve(grid, 20, 100)
+    print("RESULT PART TWO: ", num_cheats)
 
 
 if __name__ == "__main__":
-    test()
     main()
